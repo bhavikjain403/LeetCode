5dd602f9de409fe731efd7fe8de27a52f0d44457
Bhavik Jain <91948442+bhavikjain403@users.noreply.github.com>
2022-08-18 13:26:01 +0530

    Time: 65 ms (61.92%), Space: 15.3 MB (93.20%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.py
+++ 113-path-sum-ii/113-path-sum-ii.py
@@ -8,34 +8,6 @@ class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
         if not root:
             return []
-        
-        # Initialize variables
-        result = []  
-        deq = deque()
-        
-        # Start traversal
-        # Every element of deque is a state describing current node
-        # As a state we need the following: node itself, remaining sum, path from the root
-        deq.append((root, targetSum, []))
-        
-        # Breath-first-search part
-        while deq:
-            node, left_sum, path = deq.popleft()
-            
-            left_sum -= node.val
-            path.append(node.val)
-            
-            if node.left or node.right:  # Try to go into children
-                if node.left:
-                    deq.append((node.left, left_sum, path.copy()))
-                    
-                if node.right:
-                    deq.append((node.right, left_sum, path.copy()))
-            elif left_sum == 0:  # End of a traversal, check if the total path sum is equal to 0
-                result.append(path)  # Put path values into the result
-        
-        return result
-    
         ans=[]
         q=deque()
         q.append((root,targetSum,[]))
