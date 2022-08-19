b1624c898e73732682a6ce5df08091f075f47e19
Bhavik Jain <91948442+bhavikjain403@users.noreply.github.com>
2022-08-19 12:09:05 +0530

    Time: 51 ms (53.63%), Space: 13.9 MB (10.31%) - LeetHub

---
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        ans, pos = 0, 0
+        for letter in reversed(columnTitle):
+            digit = ord(letter)-64
+            ans += digit * 26**pos
+            pos += 1
+        return ans
