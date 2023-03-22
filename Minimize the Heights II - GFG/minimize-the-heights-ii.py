da0123da53967bafe4f5b1250bc249980bd9f952
Bhavik Jain <91948442+bhavikjain403@users.noreply.github.com>
2023-03-22 16:59:17 +0530

    Added solution - LeetHub

---
+++ Minimize the Heights II - GFG/minimize-the-heights-ii.py
@@ -0,0 +1,31 @@
+#User function Template for python3
+
+class Solution:
+    def getMinDiff(self, arr, n, k):
+        ans=max(arr)-min(arr)
+        arr.sort()
+        small,large=arr[0]+k,arr[n-1]-k
+        for i in range(1,n):
+            if arr[i]<k:
+                continue
+            minH=min(small,arr[i]-k)
+            maxH=max(arr[i-1]+k,large)
+            ans=min(ans,maxH-minH)
+        return ans
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        k = int(input())
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        ans = ob.getMinDiff(arr, n, k)
+        print(ans)
+        tc -= 1
+
+# } Driver Code Ends
