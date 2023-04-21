4c7066ba3771e1702200917123b964f5bcecd7e0
Bhavik Jain <91948442+bhavikjain403@users.noreply.github.com>
2023-04-21 09:35:02 +0530

    Time: 1350 ms (77.14%), Space: 14.3 MB (81.90%) - LeetHub

---
+++ 0879-profitable-schemes/0879-profitable-schemes.py
@@ -0,0 +1,9 @@
+class Solution:
+    def profitableSchemes(self, n: int, minProfit: int, group: List[int], profit: List[int]) -> int:
+        dp = [[0] * (n + 1) for i in range(minProfit + 1)]
+        dp[0][0] = 1
+        for p, g in zip(profit, group):
+            for i in range(minProfit, -1, -1):
+                for j in range(n - g, -1, -1):
+                    dp[min(i + p, minProfit)][j + g] += dp[i][j]
+        return sum(dp[minProfit]) % (10**9 + 7)
