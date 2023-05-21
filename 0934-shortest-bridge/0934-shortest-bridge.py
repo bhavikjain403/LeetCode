7d89f937463747dad78190f534103764f6ad00a9
Bhavik Jain <91948442+bhavikjain403@users.noreply.github.com>
2023-05-21 17:53:10 +0530

    Time: 390 ms (71.94%), Space: 17.8 MB (60.08%) - LeetHub

---
+++ 0934-shortest-bridge/0934-shortest-bridge.py
@@ -0,0 +1,27 @@
+class Solution:
+    def shortestBridge(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        start_i, start_j = next((i, j) for i in range(m) for j in range(n) if grid[i][j])
+        stack = [(start_i, start_j)]
+        visited = set(stack)
+        while stack:
+            i, j = stack.pop()
+            visited.add((i, j))  
+            for ii, jj in (i-1, j), (i, j-1), (i, j+1), (i+1, j):
+                if 0 <= ii < m and 0 <= jj < n and grid[ii][jj] and (ii, jj) not in visited:
+                    stack.append((ii, jj))
+                    visited.add((ii, jj))
+        
+        ans = 0
+        queue = list(visited)
+        while queue:
+            new_queue = []
+            for i, j in queue:
+                for ii, jj in (i-1, j), (i, j-1), (i, j+1), (i+1, j):
+                    if 0 <= ii < m and 0 <= jj < n and (ii, jj) not in visited:
+                        if grid[ii][jj] == 1:
+                            return ans
+                        new_queue.append((ii, jj))
+                        visited.add((ii, jj))
+            queue = new_queue
+            ans += 1
