ab5dd71fc1d4f1db2166fbf5b071e642f1896530
Bhavik Jain <91948442+bhavikjain403@users.noreply.github.com>
2023-09-03 20:04:35 +0530

    Time: 749 ms (73.28%), Space: 21.9 MB (77.59%) - LeetHub

--- 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.py
+++ 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.py
@@ -1,5 +1,29 @@
 class Solution:
     def maxSideLength(self, mat: List[List[int]], threshold: int) -> int:
+        # pre=[]
+        # m,n=len(mat),len(mat[0])
+        # for i in range(m+1):
+        #     pre.append([0]*(n+1))
+        # for i in range(1,m+1):
+        #     for j in range(1,n+1):
+        #         pre[i][j]=pre[i-1][j]+pre[i][j-1]-pre[i-1][j-1]+mat[i-1][j-1]
+        # lo,hi=0,min(m,n)
+        # while lo<=hi:
+        #     mid=(lo+hi)//2
+        #     flag=0
+        #     for i in range(mid,m+1):
+        #         for j in range(mid,n+1):
+        #             if(pre[i][j]+pre[i-mid][j-mid]-pre[i-mid][j]-pre[i][j-mid]<=threshold):
+        #                 flag=1
+        #                 break
+        #         if flag:
+        #             break
+        #     if flag:
+        #         lo=mid+1
+        #     else:
+        #         hi=mid-1
+        # return hi
+        
         pre=[]
         m,n=len(mat),len(mat[0])
         for i in range(m+1):
@@ -13,7 +37,7 @@ class Solution:
             flag=0
             for i in range(mid,m+1):
                 for j in range(mid,n+1):
-                    if(pre[i][j]+pre[i-mid][j-mid]-pre[i-mid][j]-pre[i][j-mid]<=threshold):
+                    if pre[i][j]+pre[i-mid][j-mid]-pre[i-mid][j]-pre[i][j-mid]<=threshold:
                         flag=1
                         break
                 if flag:
@@ -23,3 +47,4 @@ class Solution:
             else:
                 hi=mid-1
         return hi
+        
